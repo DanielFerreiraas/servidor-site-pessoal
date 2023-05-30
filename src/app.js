1c0f8ac28f7ff001eee9cda115ff1b13127889f0
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-05-30 03:49:32 -0300

    corrigindo tratamento de erros no servidor

--- src/app.js
+++ src/app.js
@@ -1,4 +1,6 @@
 require('./config/dotenv');
+require('express-async-errors');
+
 const express = require('express');
 
 const { initDatabase } = require('./config/db');
@@ -27,6 +29,11 @@ app.use('/api/informacoes', informacoesRoute);
 app.use('/api/auth', authRoute);
 
 initDatabase();
+
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send('Algo deu errado');
+});
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
