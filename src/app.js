e4d13ead552b4885ee422e04b10fe2b5a61a84b5
Daniel Ferreira <daniel.ferreirass.dev@gmail.com>
2023-05-30 03:53:05 -0300

    passagem de erros explicita no back-end

--- src/app.js
+++ src/app.js
@@ -32,7 +32,7 @@ initDatabase();
 
 app.use((err, req, res, next) => {
     console.error(err.stack);
-    res.status(500).send('Algo deu errado');
+    res.status(500).send({'Error': err.message});
 });
 
 app.listen(port, () => {
